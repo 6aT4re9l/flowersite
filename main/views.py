08c34093541b8f6c88c74d6b4fac1198bba8de5a
6aT4re9l <vlad-marchenko-03@mail.ru>
2024-03-07 22:15:21 +0300

    2 commit basedata

--- main/views.py
+++ main/views.py
@@ -1,19 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+﻿from django.shortcuts import render
+from goods.models import Categories
 
 
 def index(request):
+    categories = Categories.objects.all()
     context = {
-        "title": 'Главная страница',
-        "content": 'Магазин цветов',
+        "title": "Главная страница",
+        "content": "Магазин цветов",
+        "categories": categories,
     }
     return render(request, "index.html", context)
 
 
 def about(request):
     context = {
-        "title": 'О нас',
-        "content": 'Инфа обо мне и магазе',
+        "title": "О нас",
+        "content": "Инфа обо мне и магазе",
         "text_page": "Ваще крутой магаз, покупай 100 цветов",
     }
     return render(request, "about.html", context)
