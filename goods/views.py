cb35e1d0d1ac5e9112f93f44de63eeac521d5928
6aT4re9l <vlad-marchenko-03@mail.ru>
2024-03-10 14:47:02 +0300

    4 commit register tag and image for card

--- goods/views.py
+++ goods/views.py
@@ -6,10 +6,14 @@ def catalog(request):
 
     goods = Products.objects.all()
 
-    context = {"title": "Каталог", "goods": goods}
+    context = {
+        "title": "Каталог",
+        "goods": goods,
+    }
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    context = {"title": "Продукт"}
-    return render(request, "goods/product.html", context)
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    context = {"product": product}
+    return render(request, "goods/product.html", context=context)
